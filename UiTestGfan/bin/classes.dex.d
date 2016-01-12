1d4f3891600bf44bd5e24f6dd0e086fec77e098d
txh7279@163.com <txh7279@163.com>
2016-01-13 00:40:51 +0800

    init data

--- UiTestGfan/bin/classes.dex.d
+++
@@ -1,3 +0,0 @@
-/home/klisly/git/UiautomatorTestGfan/UiTestGfan/bin/classes.dex : \
-/home/klisly/git/UiautomatorTestGfan/UiTestGfan/bin/classes/com/txh/uitestgfan/UiAutomatorHelper.class \
-/home/klisly/git/UiautomatorTestGfan/UiTestGfan/bin/classes/com/txh/uitestgfan/UiTestGfan.class \
